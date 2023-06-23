78e218064eea04d8efe6073ce6bad31192da641b
GG1GG <>
2023-06-23 17:32:44 +0300

    1

---
+++ dz_19_21_23.cs
@@ -0,0 +1,57 @@
+﻿/*
+//Задача 19
+Console.WriteLine("Введите 5ти значное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
+
+//Задача 21
+
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
+// Задача 23
+
+int numbe = ReadInt("Введите цифру N: ");
+
+for (int i = 1; i <= numbe; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+int ReadInt(string messag)
+{
+    Console.Write(messag);
+    return Convert.ToInt32(Console.ReadLine());
+}
